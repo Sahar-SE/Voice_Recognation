6056feea62d18b1a6e1cf537d6aa986b25a6b60f
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-07-04 17:09:32 +0430

    add js files

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ recognition.onstart =  () => {
   instructions.text("Voice Recognition is on!")
 }
 
+// when the the recognition is ended
+
+recognition.onspeechend = () => {
+  instructions.text("Recognition stopped")
+}
+
+// When an error occured
+
+recognition.onerror = () => {
+  instructions.text("There is an error!")
+}
+
+// During the recording
+
+recognition.onresult = (event) => {
+}
+
+
 
 $("#start-btn").click((event)=> {
   if(content.length) {
