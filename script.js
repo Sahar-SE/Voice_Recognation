78559c869c0cb1c35f2b4886bce90aa8950f727d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-07-04 14:45:47 +0430

    add html files

---
+++ script.js
@@ -0,0 +1,11 @@
+var speechRecognition = window.webkitSpeechRecognition
+
+var recognition = new speechRecognition()
+
+var textbox = $("#textbox")
+
+var instructions = $("#instructions")
+
+var content = ''
+
+recognition.continuous = true
