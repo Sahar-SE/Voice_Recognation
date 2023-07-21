2cee5232dedf655a00017d1c522e377d51522c7b
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-07-22 00:14:08 +0430

    add js files

--- recommend.js
+++ recommend.js
@@ -7,6 +7,8 @@ $("#back-btn").click((event)=> {
 // handling Introduction Button
 
 $("#intro-btn").click((event)=> {
+  const message = new SpeechSynthesisUtterance("Hello, I am Rooboo I have been created by Sahar Sabah Amiri at 2023. I am a voice recognation bot. I can record your voice and read it for you again. You can use me for writing a book, or an essay. I can tell you a joke, or a qoute. I can also tell you the time and so many incredible things. I am still under development and this is just a basic version of me. Sahar will develop a new version of me very soon!");
+  speechSynthesis.speak(message);
 })
 
 // handling Author Button
