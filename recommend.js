a51b26f51b7f66d82e0759dff2509e04e6fa5de1
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-07-22 15:03:33 +0430

    add js files

--- recommend.js
+++ recommend.js
@@ -28,7 +28,7 @@ $("#time-btn").click((event)=> {
 // handling Introduction Button
 
 $("#intro-btn").click((event)=> {
-  const message = new SpeechSynthesisUtterance("Hello, I am Rooboo I have been created by Sahar Sabah Amiri at 2023. I am a voice recognation bot. I can record your voice and read it for you again. You can use me for writing a book, or an essay. I can tell you a joke, or a qoute. I can also tell you the time and so many incredible things. I am still under development and this is just a basic version of me. Sahar will develop a new version of me very soon!");
+  const message = new SpeechSynthesisUtterance("Hello, I am Rooboo I have been created by Sahar Sabah Amiri at 2023. I am a voice recognation bot. I can record your voice and read it for you again. You can use me for writing a book, or an essay. I can tell you a joke, or a qoute. I can also tell you the time and so many incredible things. I have mind but I don't have storage to store your text. I am still under development and this is just a basic version of me. Sahar will develop a new version of me very soon!");
   speechSynthesis.speak(message);
 })
 
