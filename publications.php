f5c0554e9073bdadd055abd7340175d6f5f6fe67
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-01 23:40:29 +0430

    Add comments for code readablity

--- publications.php
+++ publications.php
@@ -17,25 +17,23 @@
     <a href="index.html"><img src="imgs/house.png" class="absolute top-4 left-4 w-8 h-8 rounded-full"/></a>
   </div>
 
+  <!-- Fetching data from database -->
 
     <div class='flex flex-col justify-center items-center mt-8 font-sans'>
      <?php
         $query = "SELECT * FROM article";
         $result = mysqli_query($connect, $query);
         while($row = mysqli_fetch_assoc($result)) {
-            echo "<div class='bg-white shadow-xl rounded px-8 pt-6 pb-8 m-4 w-5/6 md:w-2/4 relative'>
-            
+            echo "
+            <div class='bg-white shadow-xl rounded px-8 pt-6 pb-8 m-4 w-5/6 md:w-2/4 relative'>
               <img src='imgs/giphy_s.gif' class='w-16 h-16 absolute right-0 top-0'/>   
                 <div class='flex'>
                   <p class='text-slate-500 font-medium p-4 pt-8 italic'>Author:</p>
                   <p class='pt-8 pr-5 pb-5 font-mono italic'>  ". $row["author"] ."</p>
                 </div>
-              
-            
               <p class='pl-4 pb-4 text-slate-500 italic'>" . $row["passage"] . "</p>
               <img src='imgs/m.png' class='absolute left-0 bottom-1 w-28 h-28'/>
-            </div>";
-            
+            </div>";    
         }
         mysqli_close($connect);
         ?>
