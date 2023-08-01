2d28aed0228a941674edac3f823df7a7c7c50678
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-01 23:35:18 +0430

    Add comments for code readablity

--- insertionData.php
+++ insertionData.php
@@ -6,11 +6,15 @@
   // Accessing form names by POST super global variable
   $name = $_POST['name'];
   $message = $_POST['message'];
+
+  // Query
   $sql="INSERT INTO article (author, passage) VALUES (?,?) ";
   $stmt = mysqli_stmt_init($connect);
   if(! mysqli_stmt_prepare($stmt, $sql)){
     die(mysqli_error($connect));
   }
+  
+  // Avoid SQL injection
   mysqli_stmt_bind_param($stmt, "ss", $name, $message);
   mysqli_stmt_execute($stmt);
   mysqli_close($connect);
