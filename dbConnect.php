a712a16be3d61c8d4d3ae0bfc6e52d314ea16236
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-01 23:28:53 +0430

    Add comments for code readablity

--- dbConnect.php
+++ dbConnect.php
@@ -1,10 +1,10 @@
-<?php
-      
-      $connect=mysqli_connect("localhost","root","","publictions");
+<?php 
+  
+  // Accessing the Database server, username, password and database name
+  $connect=mysqli_connect("localhost","root","","publictions");
 
-      if(mysqli_connect_errno()){
-        die("Database Connection Failed".mysqli_connect_error());
-      }
-      
-
+  // If connection fail it will show the error
+    if(mysqli_connect_errno()){
+      die("Database Connection Failed".mysqli_connect_error());
+    }
 ?>
